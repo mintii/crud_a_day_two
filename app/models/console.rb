873ed3c72c0f71e14568bf4b23de318f77da396b
Brittney Braxton <mintii@users.noreply.github.com>
2016-06-07 16:28:28 -0500

    Fix associations. Through is plural -_-

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,4 +1,4 @@
 class Console < ActiveRecord::Base
-  has_many :videogames, through: :catalog
-  has_many :catalogs, foregin_key: :console_id
+  has_many :catalogs, foreign_key: :console_id
+  has_many :videogames, through: :catalogs
 end
